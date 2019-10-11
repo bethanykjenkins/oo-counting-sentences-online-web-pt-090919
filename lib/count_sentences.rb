673b100c1d1ac5b67198cd8e496b3bde8427a477
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-11 02:16:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,14 +10,17 @@ class String
   end
 
   def question?
-    if self.end_with(".")
+    if self.end_with("?")
       return true 
     else
       return false
   end
 
   def exclamation?
-
+if self.end_with(".")
+      return true 
+    else
+      return false
   end
 
   def count_sentences
